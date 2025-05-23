c29161666940df3558768e7f21ff43998d15610d
Chakradhar <chakradhar.potluru@codeverse.in>
2025-05-23 22:57:15 +0530

    bug fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,13 +26,12 @@ app.use(
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res)=>{
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-})
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 
